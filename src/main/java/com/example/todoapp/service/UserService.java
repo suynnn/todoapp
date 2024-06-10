a41047fd539c7b07f37b20e2d85cf276f0313cd0
suynnn <suy00065@gmail.com>
2024-06-10 15:35:53 +0900

    test

---
+++ src/main/java/com/example/todoapp/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.todoapp.service;
+
+import com.example.todoapp.entity.User;
+import com.example.todoapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
